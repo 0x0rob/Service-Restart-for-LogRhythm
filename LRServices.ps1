f9820c547869d544b83347e2a16f1e791aa873cb
roblowery <robert.lowery.5@gmail.com>
2019-06-03 12:10:13 +0100

    Create LRServices.ps1

---
+++ LRServices.ps1
@@ -0,0 +1,22 @@
+﻿Restart-Service -Name LRAIEEngine
+Restart-Service -Name LRAIEComMgr
+Restart-Service -Name LogRhythmWebConsoleAPI
+Restart-Service -Name LogRhythmWebConsoleUI
+Restart-Service -Name LogRhythmWebIndexer
+Restart-Service -Name LogRhythmWebServicesHostAPI
+
+$caption="Restart Mediator"
+$message="Choices:"
+$choices = @("&Yes","&No")
+
+$choicedesc = New-Object System.Collections.ObjectModel.Collection[System.Management.Automation.Host.ChoiceDescription] 
+$choices | foreach  { $choicedesc.Add((New-Object "System.Management.Automation.Host.ChoiceDescription" -ArgumentList $_))} 
+
+
+$prompt = $Host.ui.PromptForChoice($caption, $message, $choicedesc, 0)
+
+Switch ($prompt)
+     {
+       0 {Restart-Service -Name scmedsvr}
+       1 {Write-Host "Not Restarting Mediator"}
+     }
